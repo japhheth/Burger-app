b76259f8cc53db9cd070cfc150b19fd5de97d619
japhheth <japhheth@gmail.com>
2020-05-14 08:05:19 +0100

    added my navigation links and created a sidedrawer component for media screens

---
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationItem from '../NavigationItem/NavigationItem';
+import classes from './NavigationItems.css'
+
+const navigationItems = () => (
+
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>Burger builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
+
+);
+
+export default navigationItems;
--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../../Components/Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolBar = () => (
  <header className={classes.Toolbar}>
      <div>MENU</div>
      <Logo/>
      <nav>
-         ...
+         <NavigationItems/>
      </nav>
  </header>
 
